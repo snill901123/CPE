68c38fef31da06c93d448ced7a548eeacf30fb9d
snill901123 <82087396+snill901123@users.noreply.github.com>
2021-09-27 18:36:43 +0800

    Add files via upload

---
+++ Rotating Sentences.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	vector<string>sen;
+	string str;
+	int max_len=0;
+	while(getline(cin,str)){
+		if(str.length()>max_len){
+			max_len=str.length();
+		}else{
+			int str_len=str.length();
+			for(int i=1;i<=max_len-str_len;i++){
+				str.push_back('&');
+			}
+		}
+		sen.push_back(str);
+	}
+	for(int i=sen.size()-1;i>=0;i--){
+		cout<<sen[i]<<endl;
+	}
+	/*for(int i=0;i<max_len;i++){
+		for(int j=sen.size()-1;j>=0;j--){
+			if(sen[j].length()>=i){
+				cout<<sen[j][i];
+			}else{
+				continue;
+			}
+		}
+		cout<<endl;
+	}*/
+}
